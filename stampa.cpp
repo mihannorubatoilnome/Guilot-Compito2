dc9999e44816c37557d1bd557bdbed26d62b81a7
Edmard <guilot.edmard@itismeucci.com>
2022-03-03 09:50:08 +0100

    Errore risolto in stampa

--- stampa.cpp
+++ stampa.cpp
@@ -8,14 +8,14 @@ Edmard Guilot 03/03/2022                   */
 
 using namespace std;
 
-int main()
+int main ()
 {
   int n = 0;
-  cout<<"Quante volte vuoi stampare la frase? : ";
-  cin>>n;
+  cout << "Quante volte vuoi stampare la frase? : ";
+  cin >> n;
 
   for (int i = 0; i < n; i++)
   {
-   cout<<"Bada come la fuma\n"<<endl; 
+   cout << "Bada come la fuma\n" << endl; 
   }
 } 
